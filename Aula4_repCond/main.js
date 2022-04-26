b307045ef98628c46e01db79891a4e246e5ab229
GustavoHCpunk <gustavo@henrimichel.com.br>
2022-04-25 23:02:39 -0300

    estudando estruturas de repetição

--- Aula4_repCond/main.js
+++ Aula4_repCond/main.js
@@ -1,13 +1,52 @@
-/* vetores são um tipo de lista, ou matriz de variáveis, onde cada
-variávle possui um indice. Os valores são de vários tipos. 
-*/
-var vetor = prompt('teste');
-function muda(texto){
-    document.getElementById("saida").innerHTML = texto; 
+
+var jogador1 = prompt("pontos do Jogar 1");
+var jogador2 = prompt("Jogador 2");
+
+
+
+function muda(texto1, texto2){
+    texto1 = parseInt(texto1);
+    texto2 = parseInt(texto2);
+    while(texto1 < 0 || texto2 < 0){ // pode ser feito com if para ser aceito e poder passar no teste.
+        if(texto1 > texto2){
+            document.getElementById("saida").innerHTML = `jogador1 fez ${jogador1} pontos`;
+            console.log(texto1); 
+        }else if(texto1 == texto2){
+            document.getElementById("saida").innerHTML = `Ninguem fez pontos pois ${jogador1} e ${jogador2} empataram!!!`;
+            console.log(texto1); 
+        }else{
+            document.getElementById("saida").innerHTML = `jogador1 fez ${jogador2} pontos`;
+            console.log(texto2); 
+        }
+        
+        break;   
+    
+    }
+    // testando ternário
+    jogador1 != 0 && jogador2 != 0 ? document.getElementById("saida2").innerHTML = parseInt(texto1)+parseInt(texto2)/2: parseInt(texto1) + parseInt(texto2); // deu certo
 }
 // no getElement tem que ser aspas duplas.
 
-muda(vetor);
-console.log(vetor);
+// switch case;
+var lista = prompt(" digite um numero");
+lista = parseInt(lista);
+switch(lista){
+    case 1: 
+    document.getElementById("saida").innerHTML = `jogador1 fez ${jogador1} switch`;
+    break;
+    case 2:  
+    document.getElementById("saida").innerHTML = `jogador1 fez ${jogador2} switch`;
+    break;
+    case 3:
+    document.getElementById("saida").innerHTML = `jogador1 fez ${jogador1+ jogador2}  switch`;
+    break;
+    default:
+    document.getElementById("saida").innerHTML = `nenhum dos casos`;
+    break;
+
+
+}
+muda(jogador1,jogador2);
+
 
 
